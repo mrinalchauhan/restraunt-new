f3dde8b22a2a58a4bf56fc7aa10a276407939f4a
Mrinal Chauhan <mrinalchauhan18.com>
2024-07-08 11:38:48 +0530

    pricing color chnged

--- src/components/cards/combo/index.jsx
+++ src/components/cards/combo/index.jsx
@@ -5,8 +5,8 @@ const ComboCard = ({ img, pricing, title }) => {
         <div className="text-center px-6 items-center">
             <div className="combo-card bg-gradient-to-b from-gray-700 to-gray-800 bg-opacity-80 rounded-lg overflow-hidden flex flex-col items-center p-8 transition-transform duration-300 transform hover:scale-105">
                 <img src={img} alt="Combo 3" className="rounded-full h-56 w-52 object-cover mb-4" />
-                <p className="text-lg text-white font-semibold text-center">Rs {pricing}</p>
-                <p className="text-lg text-white font-semibold text-center">{title}</p>
+                <p className="text-lg text-red-500 font-bold text-center">Rs {pricing}</p>
+                <p className="text-lg text-white font-bold text-center">{title}</p>
             </div>
         </div>
     )
